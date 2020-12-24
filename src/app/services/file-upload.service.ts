de5b7f5c32484e5293dcb239941503848d95dbab
mdperezr13 <mdperezr13@gmail.com>
2020-12-23 22:42:36 -0600

    snackbar

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpErrorResponse, HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,10 @@ import { HttpErrorResponse, HttpClient } from '@angular/common/http';
 
 export class FileUploadService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    ) { }
 
   sendFile(endpoint, formData, httpOptions): Observable<any> {
     return this.http.post(endpoint, formData, httpOptions).pipe(
@@ -24,6 +28,11 @@ export class FileUploadService {
       errorMessage = error.error.message;
     } else {
       // Get server-side error
+      this.snackBar.open(error.error.mensaje, 'Cerrar', {
+        duration: 10000,
+        horizontalPosition: 'end',
+        verticalPosition: 'top'
+      });
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
     console.log(errorMessage);
