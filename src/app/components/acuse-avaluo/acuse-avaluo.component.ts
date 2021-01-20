bec31d7e5078c36aa54890424fdd7b4489056d9b
mdperezr13 <mdperezr13@gmail.com>
2021-01-19 19:32:00 -0600

    consumo de servicio

--- src/app/components/acuse-avaluo/acuse-avaluo.component.ts
+++ src/app/components/acuse-avaluo/acuse-avaluo.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '@env/environment';
+import { AuthService } from '@serv/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-acuse-avaluo',
@@ -6,10 +13,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acuse-avaluo.component.css']
 })
 export class AcuseAvaluoComponent implements OnInit {
+  endpoint = environment.endpoint + 'bandeja-entrada/acuseAvaluo';
+  loading = false;
+  httpOptions;
+  noUnico;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private auth: AuthService,
+    public dialog: MatDialog,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.noUnico = this.route.snapshot.paramMap.get('no_unico');
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: this.auth.getSession().token
+      })
+    };
+    this.getData();
+  }
+
+  getData(): void {
+    this.loading = true;
+    this.http.get(this.endpoint + '?no_unico=' + this.noUnico,
+      this.httpOptions).subscribe(
+        (res: any) => {
+          this.loading = false;
+          console.log(res);
+        },
+        (error) => {
+          this.loading = false;
+          this.snackBar.open(error.error.mensaje, 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'end',
+            verticalPosition: 'top'
+          });
+        });
   }
 
 }
