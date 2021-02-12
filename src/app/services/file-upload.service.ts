cb376a8e535facbf21f679e6c88623f7ff7fcb4d
mdperezr13 <mdperezr13@gmail.com>
2021-02-11 20:54:25 -0600

    fix message subir avaluo

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -37,21 +37,13 @@ export class FileUploadService {
     } else {
       // Get server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-      if(!(error.error.mensaje instanceof Array)){
-        this.snackBar.open(error.error.mensaje, 'Cerrar', {
-          duration: 10000,
-          horizontalPosition: 'end',
-          verticalPosition: 'top'
-        });
-      }else{
-          const dialogRef = this.dialog.open(DialogValidacionesXML, {
-            width: '600px',
-            data: error.error.mensaje
-          });
-          dialogRef.afterClosed().subscribe(result => {
-            window.location.reload();            
-          });     
-      }    
+      const dialogRef = this.dialog.open(DialogValidacionesXML, {
+        width: '600px',
+        data: error.error.mensaje
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        window.location.reload();            
+      });   
     }
     return throwError(errorMessage);
   }
