a72900fd66fdbd833e58323955544416258361e5
mdperezr13 <mdperezr13@gmail.com>
2021-05-05 11:45:23 -0500

    vars iniciales

--- src/app/components/investigacion-mercado/investigacion-mercado.component.ts
+++ src/app/components/investigacion-mercado/investigacion-mercado.component.ts
@@ -1,11 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@env/environment';
+import { AuthService } from '@serv/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import * as moment from 'moment';
 
+export interface Filtros {
+  fecha_ini: Date;
+  fecha_fin: Date;
+  region: string;
+  manzana: string;
+  tipo: number;
+  alcaldia: number;
+  colonia: number;
+}
 @Component({
   selector: 'app-investigacion-mercado',
   templateUrl: './investigacion-mercado.component.html',
   styleUrls: ['./investigacion-mercado.component.css']
 })
 export class InvestigacionMercadoComponent implements OnInit {
+  endpoint = environment.endpoint;
+  pagina = 1;
+  total = 0;
+  loading = false;
+  dataSource = [];
+  displayedColumns: string[] = ['alcaldia', 'colonia', 'region', 'manzana', 'ubicacion', 'descripcion', 'precios_solicitados', 'superficie', 'vu', 'tipo'];
+  httpOptions;
+  filtros: Filtros = {} as Filtros;
+  isBusqueda;
+  queryParamFiltros;
+  @ViewChild('paginator') paginator: MatPaginator;
 
   constructor() { }
 
