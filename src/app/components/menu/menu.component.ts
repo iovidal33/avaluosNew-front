91ce87d364cfb206b6d0f573c36fed4e577d61c3
mdperezr13 <mdperezr13@gmail.com>
2021-05-07 11:41:15 -0500

    investigacion mercado solo revisor

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from '@serv/auth.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  isRevisor = false;
+  session;
 
   menus: any[] = [
     { nombre: 'Dashboard', ruta: '/dashboard', icono: 'dashboard' },
@@ -16,6 +18,12 @@ export class MenuComponent implements OnInit {
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.session = this.authService.getSession();
+    if(this.session.userData.redirect === "bandeja-entrada"){
+      this.isRevisor = true;
+    } else {
+      this.isRevisor = false;
+    }
     this.menus = this.authService.getMenu();
   }
 
